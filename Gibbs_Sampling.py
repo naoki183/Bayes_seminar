526f8dbb367047f9f16eb0ad209b67fb8f97c399
吉田直生 <yoshidanaoki@yoshidanaokinoMacBook-Air.local>
2022-06-16 03:54:49 +0900

    ソースコードの追加

---
+++ Gibbs_Sampling.py
@@ -0,0 +1,168 @@
+import numpy as np
+import math
+from scipy.stats import wishart
+from scipy.stats import dirichlet
+import matplotlib.pyplot as plt
+import random
+
+# シード値
+random_state = 42
+# 訓練データ数
+N = 10000
+# 訓練時のクラスタ数
+K = 4
+
+np.random.seed(random_state)
+
+def gen_dataset():
+    k1 = np.random.rand(1000, 2) + 2
+    k2 = np.random.rand(1000, 2)
+    k3 = np.random.randn(1000, 2)
+    for i in range(1000):
+        k3[i, 1] += 2
+        k3[i, 0] -= 2
+    return [k1, k2, k3]
+
+def gen_train_data():
+    X = np.zeros((N, 2))
+    for i in range(N):
+        k = np.random.choice(a=[0, 1, 2], p=[1/3, 1/3, 1/3], size=1)
+        if k == 0:
+            x = np.random.rand(2) + 2
+            X[i, :] = x
+        elif k == 1:
+            x = np.random.rand(2)
+            X[i, :] = x
+        else:
+            x = np.random.randn(2)
+            x[0] -= 2
+            x[1] += 2
+            X[i, :] = x
+    
+    return X
+
+
+def calc_eta_n(x_n, mu, Lambda, pi):
+    eta_n = np.zeros(K)
+    for k in range(K):
+        mu_k = mu[k, :].reshape([2, 1])
+        x_n = x_n.reshape([2, 1])
+        Lambda_k = Lambda[k, :, :]
+        s = (-1/2) * (x_n - mu_k).T @ Lambda_k @ (x_n - mu_k)
+        eta_n[k] = math.exp(s[0][0] + (1/2) * math.log(np.linalg.det(Lambda_k)) + math.log(pi[k]))
+    
+    return eta_n / np.sum(eta_n)
+
+def calc_beta_k_m_k_nu_k_W_k(beta, s_k, X, m, W, nu):
+    beta_k = beta + np.sum(s_k)
+    s = np.zeros(2)
+    for i in range(N):
+        s += s_k[i] * X[i, :]
+    s += beta * m
+    m_k = s / beta_k
+
+    s = np.zeros((2, 2))
+    for i in range(N):
+        s += s_k[i] * X[i, :].reshape([2, 1]) @ X[i, :].reshape([1, 2])
+    
+    s += beta * m.reshape([2, 1]) @ m.reshape([1, 2]) - beta_k * m_k.reshape([2, 1]) @ m_k.reshape([1, 2]) + np.linalg.inv(W)
+    W_k = np.linalg.inv(s)
+    nu_k = np.sum(s_k) + nu
+
+    return [beta_k, m_k, nu_k, W_k]
+
+def calc_alpha_k(s_k, alpha_k):
+    alpha_k = np.sum(s_k) + alpha_k
+    return alpha_k
+
+def sample_s_n(eta_n):
+    i = np.random.choice(a=range(K), p=eta_n.tolist(), size=1)
+    s_n = np.zeros(K)
+    s_n[i] = 1
+    return s_n
+
+def sample_Lambda_k_mu_k(m_k, beta_k, nu_k, W_k):
+    Lambda_k = wishart.rvs(nu_k, W_k, size=1, random_state=random_state)
+    # Lambda_k = np.array(Lambda_k)
+    mu_k = np.random.multivariate_normal(m_k, np.linalg.inv(beta_k * Lambda_k)) 
+    return [Lambda_k, mu_k]
+
+def sample_pi(alpha):
+    pi = dirichlet.rvs(alpha, size=1)
+    return pi[0]
+
+def Gibbs_sampling(X, iter):
+    # 初期化
+    m = np.random.randn(2)
+    beta = 3
+    nu = 2
+    w = np.random.randn(2, 2)
+    W = w.T @ w
+    Lambda = np.zeros((K, 2, 2))
+    alpha = np.random.rand(K)
+    mu = np.zeros((K, 2))
+    for k in range(K):
+        Lambda_k, mu_k = sample_Lambda_k_mu_k(m, beta, nu, W)
+        Lambda[k, :, :] = Lambda_k
+        mu[k, :] = mu_k
+    pi = sample_pi(alpha)
+    s = np.zeros((N, K))
+    alpha_ = np.zeros(K)
+    # サンプリングを繰り返す
+    for i in range(iter):
+        for n in range(N):
+            eta_n = calc_eta_n(X[n, :], mu, Lambda, pi)
+            s_n = sample_s_n(eta_n)
+            s[n, :] = s_n
+        for k in range(K):
+            beta_k, m_k, nu_k, W_k = calc_beta_k_m_k_nu_k_W_k(beta, s[:, k], X, m, W, nu)
+            Lambda_k, mu_k = sample_Lambda_k_mu_k(m_k, beta_k, nu_k, W_k)
+            Lambda[k, :, :] = Lambda_k
+            mu[k, :] = mu_k
+        for k in range(K):
+            alpha_k = calc_alpha_k(s[:, k], alpha[k])
+            alpha_[k] = alpha_k
+        pi = sample_pi(alpha_)
+    
+    print(pi)
+    print(Lambda)
+    print(mu)
+    
+    return [Lambda, mu, pi]
+
+def predict_distribution(M, Lambda, mu, pi):
+    pred_data = np.zeros((M, 2))
+    for i in range(M):
+        k = np.random.choice(a=range(K), p=pi, size=1)
+        Lambda_k = Lambda[k, :, :][0]
+        mu_k = mu[k, :][0]
+        x = np.random.multivariate_normal(mu_k, np.linalg.inv(Lambda_k))
+        x = x.tolist()
+        pred_data[i, :] = x
+    return pred_data
+
+def plot_points(pred_data):
+    k1, k2, k3 = gen_dataset()
+    fig = plt.figure(figsize=(12,8))
+    ax = fig.add_subplot(111)
+    ax.scatter(k1[:, 0], k1[:, 1], label="クラスタ1", color="green")
+    ax.scatter(k2[:, 0], k2[:, 1], label="クラスタ2", color="blue")
+    ax.scatter(k3[:, 0], k3[:, 1], label="クラスタ3", color="grey")
+    ax.scatter(pred_data[:, 0], pred_data[:, 1], label="予測分布", color="red")
+    plt.legend()
+    plt.show()
+    return
+
+
+
+def main():
+    X = gen_train_data()
+    iter = 100
+    M = 3000
+    Lambda, mu, pi = Gibbs_sampling(X, iter)
+    pred_data = predict_distribution(M, Lambda, mu, pi)
+    plot_points(pred_data)
+    return
+
+if __name__ == "__main__":
+    main()
---
+++ hennbunn_suironn.py
@@ -0,0 +1,159 @@
+import numpy as np
+import math
+from scipy.special import psi
+import matplotlib.pyplot as plt
+from scipy.stats import wishart
+from scipy.stats import dirichlet
+
+# データの次元数
+D = 2
+# カテゴリ数
+K = 3
+# 訓練データ数
+N = 10000
+# イテレーション数
+iter = 100
+# シード値
+random_state = 42
+
+np.random.seed(random_state)
+
+def gen_dataset():
+    k1 = np.random.rand(1000, 2) + 2
+    k2 = np.random.rand(1000, 2)
+    k3 = np.random.randn(1000, 2)
+    for i in range(1000):
+        k3[i, 1] += 2
+        k3[i, 0] -= 2
+    return [k1, k2, k3]
+
+def gen_train_data():
+    X = np.zeros((N, 2))
+    for i in range(N):
+        k = np.random.choice(a=[0, 1, 2], p=[1/3, 1/3, 1/3], size=1)
+        if k == 0:
+            x = np.random.rand(2) + 2
+            X[i, :] = x
+        elif k == 1:
+            x = np.random.rand(2)
+            X[i, :] = x
+        else:
+            x = np.random.randn(2)
+            x[0] -= 2
+            x[1] += 2
+            X[i, :] = x
+    
+    return X
+
+def calc_Es(nu_k, W_k, m_k, beta_k):
+    E_Lambda_k = nu_k * W_k
+    E_ln_det_Lambda_k = D * math.log(2) + math.log(np.linalg.det(W_k))
+    for d in range(D):
+        E_ln_det_Lambda_k += psi((nu_k + 1 - d) / 2)
+    E_Lambda_k_mu_k = nu_k * W_k @ m_k
+    s = m_k.reshape([1, D]) @ W_k @ m_k.reshape([D, 1])
+    E_mu_k_Lambda_k_mu_k = nu_k * s[0][0] + D / beta_k
+    return [E_Lambda_k, E_ln_det_Lambda_k, E_Lambda_k_mu_k, E_mu_k_Lambda_k_mu_k]
+
+
+def update_eta(X, nu, W, m, beta, alpha):
+    Es = []
+    for k in range(K):
+        Es.append(calc_Es(nu[k], W[k, :, :], m[k, :], beta[k]))
+    eta = np.zeros((N, K))
+    for n in range(N):
+        for k in range(K):
+            E_Lambda_k, E_ln_det_Lambda_k, E_Lambda_k_mu_k, E_mu_k_Lambda_k_mu_k = Es[k]
+            x_n = X[n, :]
+            s = x_n.reshape([1, D]) @ E_Lambda_k @ x_n.reshape([D, 1])
+            t = x_n.reshape([1, D]) @ E_Lambda_k_mu_k
+            eta[n, k] = math.exp(-(1/2) * s[0][0] + t[0] - (1/2) * E_mu_k_Lambda_k_mu_k + (1/2) * E_ln_det_Lambda_k + psi(alpha[k]) - psi(np.sum(alpha)))
+        eta[n, :] = eta[n, :] / np.sum(eta[n, :])
+    
+    return eta
+
+def update_beta_k_m_k_nu_k_W_k(eta, beta, X, m, W, nu, k):
+    beta_k = beta + np.sum(eta[:, k])
+    s = np.zeros(D)
+    for n in range(N):
+        s += eta[n, k] * X[n, :]
+    m_k = (s + beta * m) / beta_k
+    W_k = np.zeros((D, D))
+    for n in range(N):
+        W_k += eta[n, k] * X[n, :].reshape([D, 1]) @ X[n, :].reshape([1, D])
+    W_k += beta * m.reshape([D, 1]) @ m.reshape([1, D]) - beta_k * m_k.reshape([D, 1]) @ m_k.reshape([1, D]) + np.linalg.inv(W)
+    W_k = np.linalg.inv(W_k)
+    nu_k = np.sum(eta[:, k]) + nu
+
+    return [beta_k, m_k, W_k, nu_k]
+
+def update_alpha(eta, alpha):
+    alpha_ = np.zeros(K)
+    for k in range(K):
+        alpha_[k] = np.sum(eta[:, k]) + alpha[k]
+    return alpha_
+
+def hennbunn_suironn(X):
+    beta = 3
+    nu = 2
+    w = np.random.randn(D, D)
+    W = w.T @ w
+    m = np.random.randn(D)
+    beta_ = np.zeros(K)
+    nu_ = np.zeros(K)
+    W_ = np.zeros((K, D, D))
+    m_ = np.zeros((K, D))
+    for k in range(K):
+        beta_[k] = 2
+        nu_[k] = 3
+        w = np.random.randn(D, D)
+        W_[k, :, :] = w.T @ w
+        m_[k, :] = np.random.randn(D)
+    alpha = np.random.rand(K) * 10
+    alpha_ = np.random.rand(K) * 10
+    eta = np.zeros((N, K))
+    for i in range(iter):
+        eta = update_eta(X, nu_, W_, m_, beta_, alpha_)
+        for k in range(K):
+            beta_[k], m_[k, :], W_[k, :, :], nu_[k] = update_beta_k_m_k_nu_k_W_k(eta, beta, X, m, W, nu, k)
+        alpha_ = update_alpha(eta, alpha)
+    
+    return [alpha_, beta_, nu_, W_, m_]
+
+
+def plot_points(pred_data):
+    k1, k2, k3 = gen_dataset()
+    fig = plt.figure(figsize=(12,8))
+    ax = fig.add_subplot(111)
+    ax.scatter(k1[:, 0], k1[:, 1], label="cluster1", color="green")
+    ax.scatter(k2[:, 0], k2[:, 1], label="cluster2", color="blue")
+    ax.scatter(k3[:, 0], k3[:, 1], label="cluster3", color="grey")
+    ax.scatter(pred_data[:, 0], pred_data[:, 1], label="prediction", color="red")
+    plt.legend()
+    plt.show()
+    return
+
+def main():
+    X = gen_train_data()
+    # サンプリングの回数
+    M = 3000
+    alpha_, beta_, nu_, W_, m_ = hennbunn_suironn(X)
+    pred_data = np.zeros((M, D))
+    for i in range(M):
+        pi = dirichlet.rvs(alpha_, size=1)
+        pi = pi[0]
+        k = np.random.choice(range(K), p=pi, size=1)
+        Lambda_k = wishart.rvs(nu_[k][0], W_[k, :, :][0], size=1, random_state=random_state)
+        mu_k = np.random.multivariate_normal(m_[k, :][0], np.linalg.inv(beta_[k][0] * Lambda_k))
+        x = np.random.multivariate_normal(mu_k, np.linalg.inv(Lambda_k))
+        pred_data[i, :] = x
+    plot_points(pred_data)
+    return
+    
+
+
+
+
+
+if __name__ == "__main__":
+    main()
